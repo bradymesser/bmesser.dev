49a518a4ec5b93caf59c328c3f9c84bcdc314e3b
William B Messer <wmesser@clemson.edu>
2021-01-24 18:10:55 -0500

    start layout

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e1e1e",
+        secondary: "#73A5A2",
+        alt: "#73A5A2",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
