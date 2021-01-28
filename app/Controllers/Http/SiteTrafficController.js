2c14b807b5a12cf98119b03f7c2f201458871507
William B Messer <wmesser@clemson.edu>
2021-01-27 20:09:52 -0500

    Add unique visitor count

--- app/Controllers/Http/SiteTrafficController.js
+++ app/Controllers/Http/SiteTrafficController.js
@@ -6,10 +6,12 @@ class SiteTrafficController {
     const stats = await SiteTraffic.all();
     var statDictionary = [];
     let iterableArray = [];
+    let visitorDictionary = []; // Used to count unique visitors
     stats.rows.forEach((element, index) => {
       statDictionary[element.page]
         ? statDictionary[element.page]++
         : (statDictionary[element.page] = 1);
+      visitorDictionary[element.ip] = 1; // Value doesn't matter
     });
 
     // Need to find a better way to do this, edge templates don't like dictionaries
@@ -17,10 +19,10 @@ class SiteTrafficController {
     for (var key in statDictionary) {
       iterableArray.push({ page: key, count: statDictionary[key] });
     }
-
     response.send(
       view.render("traffic", {
         totalTraffic: await SiteTraffic.getCount(),
+        uniqueVisitors: Object.keys(visitorDictionary).length,
         siteStats: iterableArray,
       })
     );
